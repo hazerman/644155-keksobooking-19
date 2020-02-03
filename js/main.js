2671db032e968f46526c9053e20dd540a239653a
hazerman <49827421+hazerman@users.noreply.github.com>
2020-02-04 00:20:35 +0500

    Добавляет валидацию формы

--- js/main.js
+++ js/main.js
@@ -58,6 +58,18 @@
     house: 'Дом',
     bungalo: 'Бунгало'
   };
+  var typeMinPrice = {
+    palace: 10000,
+    flat: 1000,
+    house: 5000,
+    bungalo: 0
+  };
+  var linkRoomsGuests = {
+    '1': [1],
+    '2': [1, 2],
+    '3': [1, 2, 3],
+    '100': [0]
+  };
   var map = document.querySelector('.map');
   var mapPinsArea = map.querySelector('.map__pins');
   var mapPinMain = map.querySelector('.map__pin--main');
@@ -66,7 +78,14 @@
   var mapFormSelects = mapFiltersContainer.querySelectorAll('.map__filters-container select');
   var adForm = document.querySelector('.ad-form');
   var adFormFieldsets = adForm.querySelectorAll('.ad-form fieldset');
-  var adFromAddressInput = adForm.querySelector('.ad-form [name="address"]');
+  var adFormTitleInput = adForm.querySelector('#title');
+  var adFromAddressInput = adForm.querySelector('#address');
+  var adFormTypeSelect = adForm.querySelector('#type');
+  var adFormPriceInput = adForm.querySelector('#price');
+  var adFormTimeInSelect = adForm.querySelector('#timein');
+  var adFormTimeOutSelect = adForm.querySelector('#timeout');
+  var adFormRoomNumberSelect = adForm.querySelector('#room_number');
+  var adFormCapacitySelect = adForm.querySelector('#capacity');
   var mapPinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
   var mapCardTemplate = document.querySelector('#card').content.querySelector('.map__card');
   var pinMaxX = mapPinsArea.offsetWidth;
@@ -84,6 +103,46 @@
     for (var i = 0; i < collection.length; i++) {
       collection[i].removeAttribute('disabled');
     }
+  };
+
+  var setLinkBetweenTypeAndPrice = function () {
+    adFormPriceInput.setAttribute('min', typeMinPrice[adFormTypeSelect.value]);
+    adFormPriceInput.setAttribute('placeholder', typeMinPrice[adFormTypeSelect.value]);
+  };
+
+  var setLinkBetweenTime = function (action) {
+    if (action === 'in') {
+      adFormTimeOutSelect.value = adFormTimeInSelect.value;
+    } else {
+      adFormTimeInSelect.value = adFormTimeOutSelect.value;
+    }
+  };
+
+  var getValidityMessageForCapacity = function (capacity) {
+    var message = '';
+    var roomNumber = adFormRoomNumberSelect.value;
+    for (var i = 0; i < linkRoomsGuests[roomNumber].length; i++) {
+      if (parseInt(capacity.value, 10) === linkRoomsGuests[roomNumber][i]) {
+        return message;
+      }
+    }
+    if (parseInt(roomNumber, 10) === 100) {
+      message = 'Для такого количества комнат нужно выбрать вариант НЕ ДЛЯ ГОСТЕЙ';
+      return message;
+    }
+    message =
+      'Для такого количества комнат вы можете выбрать не больше '
+      + linkRoomsGuests[roomNumber][linkRoomsGuests[roomNumber].length - 1]
+      + ' гостей, а также нельзя выбирать вариант НЕ ДЛЯ ГОСТЕЙ';
+    return message;
+  };
+
+  var showInvalidField = function (field) {
+    field.style.boxShadow = '0 0 0 5px red';
+  };
+
+  var resetInvalidField = function (field) {
+    field.removeAttribute('style');
   };
 
   var getAddressFromMainPin = function (isActive) {
@@ -237,10 +296,59 @@
     }
   });
 
+  adFormTitleInput.addEventListener('invalid', function (evt) {
+    showInvalidField(evt.target);
+  });
+
+  adFormTitleInput.addEventListener('blur', function (evt) {
+    if (evt.target.validity.valid) {
+      resetInvalidField(evt.target);
+    }
+  });
+
+  adFormPriceInput.addEventListener('invalid', function (evt) {
+    showInvalidField(evt.target);
+  });
+
+  adFormPriceInput.addEventListener('blur', function (evt) {
+    if (evt.target.validity.valid) {
+      resetInvalidField(evt.target);
+    }
+  });
+
+  adFormTypeSelect.addEventListener('input', function () {
+    setLinkBetweenTypeAndPrice();
+  });
+
+  adFormTimeInSelect.addEventListener('input', function () {
+    setLinkBetweenTime('in');
+  });
+
+  adFormTimeOutSelect.addEventListener('input', function () {
+    setLinkBetweenTime('out');
+  });
+
+  adFormCapacitySelect.addEventListener('input', function (evt) {
+    adFormCapacitySelect.setCustomValidity(getValidityMessageForCapacity(adFormCapacitySelect));
+    if (evt.target.validity.valid) {
+      resetInvalidField(evt.target);
+    }
+  });
+
+  adFormCapacitySelect.addEventListener('invalid', function (evt) {
+    showInvalidField(evt.target);
+  });
+
+  adFormRoomNumberSelect.addEventListener('input', function () {
+    adFormCapacitySelect.setCustomValidity(getValidityMessageForCapacity(adFormCapacitySelect));
+  });
+
   disableFormElements(mapFormInputs);
   disableFormElements(mapFormSelects);
   disableFormElements(adFormFieldsets);
   adFromAddressInput.value = getAddressFromMainPin(false);
+  setLinkBetweenTypeAndPrice();
+  adFormCapacitySelect.setCustomValidity(getValidityMessageForCapacity(adFormCapacitySelect));
 
   var cards = generateArrayOfCards(8);
   var fragmentForCard = document.createDocumentFragment();
