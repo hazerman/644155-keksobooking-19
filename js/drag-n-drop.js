8facca91bf72ecb84ec7421669dd7bf30abc4bc6
hazerman <49827421+hazerman@users.noreply.github.com>
2020-02-11 18:39:48 +0500

    Изменяет логику поведения перетаскивания при выходе курсора за границу

--- js/drag-n-drop.js
+++ js/drag-n-drop.js
@@ -4,10 +4,14 @@
   var map = document.querySelector('.map');
   var mapPinsArea = map.querySelector('.map__pins');
   var AreaLimit = {
-    MIN_Y: 130,
-    MAX_Y: 630,
-    MIN_X: 0,
-    MAX_X: mapPinsArea.offsetWidth
+    X: {
+      MIN: 0,
+      MAX: mapPinsArea.offsetWidth
+    },
+    Y: {
+      MIN: 130,
+      MAX: 630
+    }
   };
 
   var activate = function (element, width, height, callback) {
@@ -19,66 +23,35 @@
         x: evt.clientX,
         y: evt.clientY
       };
-      var overArea = {
-        minX: -Infinity,
-        maxX: Infinity,
-        minY: -Infinity,
-        maxY: Infinity,
-        isOverMinX: false,
-        isOverMaxX: false,
-        isOverMinY: false,
-        isOverMaxY: false,
-      };
       var mouseMoveHandler = function (moveEvt) {
         moveEvt.preventDefault();
         var shift = {
           x: startCoordinates.x - moveEvt.clientX,
           y: startCoordinates.y - moveEvt.clientY,
         };
-        var left = parseInt(target.style.left, 10) - shift.x;
-        var top = parseInt(target.style.top, 10) - shift.y;
+        var newStyleLeft = parseInt(target.style.left, 10) - shift.x;
+        var newStyleTop = parseInt(target.style.top, 10) - shift.y;
         startCoordinates = {
           x: moveEvt.clientX,
           y: moveEvt.clientY
         };
-        if (left < (AreaLimit.MIN_X - halfWidth)) {
-          target.style.left = (AreaLimit.MIN_X - halfWidth) + 'px';
-          if (!AreaLimit.isOverMinX) {
-            overArea.minX = moveEvt.clientX;
-            AreaLimit.isOverMinX = true;
+
+        var getPositionStyle = function (axis, newStylePosition, side) {
+          var position;
+          if (newStylePosition < (AreaLimit[axis].MIN - side)) {
+            position = (AreaLimit[axis].MIN - side) + 'px';
+            document.removeEventListener('mousemove', mouseMoveHandler);
+          } else if (newStylePosition > (AreaLimit[axis].MAX - side)) {
+            position = (AreaLimit[axis].MAX - side) + 'px';
+            document.removeEventListener('mousemove', mouseMoveHandler);
+          } else {
+            position = newStylePosition + 'px';
           }
-          startCoordinates.x = overArea.minX;
-        } else if (left > (AreaLimit.MAX_X - width / 2)) {
-          target.style.left = (AreaLimit.MAX_X - halfWidth) + 'px';
-          if (!AreaLimit.isOverMaxX) {
-            overArea.maxX = moveEvt.clientX;
-            AreaLimit.isOverMaxX = true;
-          }
-          startCoordinates.x = overArea.maxX;
-        } else {
-          target.style.left = left + 'px';
-          AreaLimit.isOverMinX = false;
-          AreaLimit.isOverMaxX = false;
-        }
-        if (top < (AreaLimit.MIN_Y - height)) {
-          target.style.top = (AreaLimit.MIN_Y - height) + 'px';
-          if (!AreaLimit.isOverMinY) {
-            overArea.minY = moveEvt.clientY;
-            AreaLimit.isOverMinY = true;
-          }
-          startCoordinates.y = overArea.minY;
-        } else if (top > (AreaLimit.MAX_Y - height)) {
-          target.style.top = (AreaLimit.MAX_Y - height) + 'px';
-          if (!AreaLimit.isOverMaxY) {
-            overArea.maxY = moveEvt.clientY;
-            AreaLimit.isOverMaxY = true;
-          }
-          startCoordinates.y = overArea.maxY;
-        } else {
-          target.style.top = top + 'px';
-          AreaLimit.isOverMinY = false;
-          AreaLimit.isOverMaxY = false;
-        }
+          return position;
+        };
+
+        target.style.left = getPositionStyle('X', newStyleLeft, halfWidth);
+        target.style.top = getPositionStyle('Y', newStyleTop, height);
         callback();
       };
 
