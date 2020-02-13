4c9fb0bd080ca9bc9434ca63c6adb8844a6b4e89
hazerman <49827421+hazerman@users.noreply.github.com>
2020-02-13 13:22:07 +0500

    Добавляет загрузку объектов карточек с сервера

--- js/main.js
+++ js/main.js
@@ -1,17 +1,14 @@
 'use strict';
 
 (function () {
-  var NUMBER_OF_CARDS = 8;
   var map = document.querySelector('.map');
   var mapPinMain = map.querySelector('.map__pin--main');
-  var cardObjects = [];
 
   var makePageActive = function () {
     mapPinMain.removeEventListener('mousedown', mapPinMainMouseDownHandler);
     mapPinMain.removeEventListener('keydown', mapPinMainEnterKeyHandler);
-    cardObjects = window.data.generateArrayOfCards(NUMBER_OF_CARDS);
     window.form.makeFormActive();
-    window.map.makeMapActive(cardObjects);
+    window.map.makeMapActive();
   };
 
   var mapPinMainMouseDownHandler = function () {
